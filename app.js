c5e5143d39ea190603cb6e491950b03ba8e43f48
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-22 17:33:03 +0800

    progress

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "manuel",
@@ -17,12 +16,36 @@ const app = Vue.createApp({
         },
       ],
     };
+  }, // end of data
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails()">Show Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "09263771993",
+        email: "manuel@localhost.com",
+      },
+    };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
-    }
+    },
   },
 });
 
-app.mount('#app');
+app.mount("#app");
